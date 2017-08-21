c15d8226dc681e005aefef9e00b3e86778f673f8
Rochdi BEY <bey.rochdi@gmail.com>
2017-08-21 15:01:05 +0200

    Add validation for ingredient

--- src/api/ingredients.js
+++ src/api/ingredients.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
+const joi = require('joi');
+const expressJoiMiddleware = require('express-joi-middleware');
 const ingredientService = require('../service/ingredientService');
 const httpStatusCodes = require('http-status-codes');
 
 function getRouter() {
 
+  //----------------------------Schema validation------------------------------
+  const createIngredientSchema = {
+      body: {
+          name: joi.string().required(),
+          proteins: joi.number().required(),
+          carbs: joi.number().required(),
+          fat: joi.number().required(),
+          calories: joi.number().required(),
+      },
+  };
 
   router.get('/ingredients', function(req, res, next) {
     ingredientService.findAll().then((result) => {
@@ -23,15 +35,27 @@ function getRouter() {
       });
   });
 
-  router.post('/ingredients', function(req, res) {
-    const body = req.body;
-    ingredientService.create(body).then((result) => {
-        res.status(httpStatusCodes.CREATED).json(result);
-    }).catch((error) => {
-        res.status(error.statusCode).json(error.message);
-        next(error);
-    });
-  });
+  router.post('/ingredients',
+    expressJoiMiddleware(
+        createIngredientSchema,
+        {
+            wantResponse: true,
+            joiOptions: {
+                abortEarly: false,
+                allowUnknown: false,
+            }
+        }
+    ),
+    function(req, res) {
+      const body = req.body;
+      ingredientService.create(body).then((result) => {
+          res.status(httpStatusCodes.CREATED).json(result);
+      }).catch((error) => {
+          res.status(error.statusCode).json(error.message);
+          next(error);
+      });
+    }
+  );
 
   router.put('/ingredients/:id', function(req, res) {
       const body = req.body;
